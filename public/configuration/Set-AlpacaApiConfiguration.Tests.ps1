d661a7194a2f85772ae86c77a8eaebce8f127bea
Nate Askoff <nate.askoff@outlook.com>
2024-04-02 17:17:08 -0400

    continue working on pester tests

--- public/configuration/Set-AlpacaApiConfiguration.Tests.ps1
+++ public/configuration/Set-AlpacaApiConfiguration.Tests.ps1
@@ -7,12 +7,9 @@ BeforeAll {
 
 Describe "Configuration" {
     Context "Set-AlpacaApiConfiguration" {
-        It "CredentialsCreated" {
+        It "CredentialsFileCreated" {
             Set-AlpacaApiConfiguration -ApiKey "TestApiKey" -ApiSecret "TestApiSecret" -Confirm:$false
             Test-Path -Path "$($HOME)/.alpaca-credentials" | Should -BeTrue
-            $config = Get-Content -Path "$($HOME)/.alpaca-credentials" | ConvertFrom-Json
-            $config.ApiKey | Should -Be [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("TestApiKey"))
-            $config.ApiSecret | Should -Be [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("TestApiSecret"))
         }
     }
 }
